0f26a8f8ed706f9572cd4bc600927e6a864448a4
yahaiii <wanekaxa@gmail.com>
2023-03-13 00:56:22 +0100

    refactor: fetch data with HttpClient

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Product } from '../models/product.model'
+import { Product } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  private productsUrl = '../../assets/data.json';
+  private dataUrl = '../../assets/data.json';
+
+  
+
   constructor(private http: HttpClient) { }
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.productsUrl);
+    return this.http.get<Product[]>(this.dataUrl);
   }
 }
