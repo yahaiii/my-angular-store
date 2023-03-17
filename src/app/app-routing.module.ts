e251edd324cd9193140c26b6abc63df80c938873
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-03-17 13:29:58 +0100

    feat: modify navigation, add back-to-home in cart
    feat: modify navigation, add back-to-home in cart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CartComponent } from './components/cart/cart.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { CartComponent } from './components/cart/cart.component';
 
 const routes: Routes = [
   { path: '', component: ProductListComponent },
-  { path: 'cart', component: CartComponent}
+  { path: 'cart', component: CartComponent }
 ];
 
 @NgModule({
