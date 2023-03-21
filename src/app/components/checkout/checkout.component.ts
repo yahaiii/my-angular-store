ab49d73fef0557545a184b51dbc1c8bf340d10fe
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-03-21 15:06:00 +0100

    feat: add sweetalert2 alerts to cart

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 import { Router } from '@angular/router';
 
 import { CartService } from '../../services/cart.service';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-checkout',
@@ -20,7 +22,7 @@ export class CheckoutComponent implements OnInit {
 
   ngOnInit() {
     this.checkoutForm = this.formBuilder.group({
-      name: new FormControl('', Validators.required),
+      name: new FormControl('', [Validators.required, Validators.minLength(5),]),
       address: new FormControl('', Validators.required),
       city: new FormControl('', Validators.required),
       state: new FormControl('', Validators.required),
@@ -35,13 +37,27 @@ export class CheckoutComponent implements OnInit {
   onSubmit() {
 
     if (this.cartService.getItems().length === 0) {
-      alert('Your cart is empty.');
+      // alert('Your cart is empty.');
+      Swal.fire({
+        icon: 'warning',
+        title: 'Your cart is empty',
+        text: 'Please add items to your cart before checking out',
+        confirmButtonText: 'OK'
+      });
       return;
     }
 
-    alert('Your order has been completed!');
+    // alert('Your order has been completed!');
+
+    Swal.fire({
+      icon: 'success',
+      title: 'Order completed!',
+      text: 'Thank you for your purchase.',
+      confirmButtonText: 'OK'
+    });
 
     this.cartService.clearCart();
     this.router.navigate(['/']);
   }
 }
+
