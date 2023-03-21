5f97fa5b22c42462ed0b85f84ba85c12f0dae449
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-03-21 14:12:34 +0100

    feat: check if cart is empty

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-checkout',
@@ -9,7 +12,11 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 export class CheckoutComponent implements OnInit {
   checkoutForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private cartService: CartService
+    ) { }
 
   ngOnInit() {
     this.checkoutForm = this.formBuilder.group({
@@ -21,11 +28,20 @@ export class CheckoutComponent implements OnInit {
       cardName: new FormControl('', Validators.required),
       cardNumber: new FormControl('', [Validators.required, Validators.pattern('[0-9]{16}')]),
       expiration: new FormControl('', [Validators.required, Validators.pattern('(0[1-9]|1[0-2])\/([0-9]{4})')]),
-      cvv: new FormControl('', [Validators.required, Validators.pattern('[0-9]{3}')])
+      cvv: new FormControl('', [Validators.required, Validators.maxLength(3), Validators.pattern('[0-9]{3}')])
     });
   }
 
   onSubmit() {
-    // handle form submission
+
+    if (this.cartService.getItems().length === 0) {
+      alert('Your cart is empty.');
+      return;
+    }
+
+    alert('Your order has been completed!');
+
+    this.cartService.clearCart();
+    this.router.navigate(['/products']);
   }
 }
