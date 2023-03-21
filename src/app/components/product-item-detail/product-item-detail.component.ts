7c1f20a5201757fcd2be2d98bc07f5460d6e0df6
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-03-21 22:43:10 +0100

    feat: implement getProductById in Product Service

--- src/app/components/product-item-detail/product-item-detail.component.ts
+++ src/app/components/product-item-detail/product-item-detail.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+// import { ActivatedRoute } from '@angular/router';
+// import { ProductService } from '../../services/product.service';
+// import { Product } from '../../models/product.model';
+
+// @Component({
+//   selector: 'app-product-item-detail',
+//   templateUrl: './product-item-detail.component.html',
+//   styleUrls: ['./product-item-detail.component.css']
+// })
+// export class ProductItemDetailComponent implements OnInit {
+//   product!: Product;
+
+//   constructor(
+//     private route: ActivatedRoute,
+//     private productService: ProductService
+//   ) { }
+
+//   ngOnInit() {
+//     const id = Number(this.route.snapshot.paramMap.get('id'));
+//     this.productService.getProductById(id).subscribe(product => {
+//       this.product = product;
+//     });
+//   }
+// }
+
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-product-item-detail',
   templateUrl: './product-item-detail.component.html',
   styleUrls: ['./product-item-detail.component.css']
 })
-export class ProductItemDetailComponent {
+export class ProductItemDetailComponent implements OnInit {
 
+  product: Product | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.productService.getProductById(id)
+        .subscribe(product => this.product = product);
+    }
+  }
 }
+
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Product } from '../models/product.model';
 
 @Injectable({
@@ -15,5 +16,16 @@ export class ProductService {
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.dataUrl);
+  }
+
+  // getProductById(id: number): Observable<Product> {
+  //   const url = `${this.dataUrl}/${id}`;
+  //   return this.http.get<Product>(url);
+  // }
+
+  getProductById(id: string): Observable<Product> {
+    return this.getProducts().pipe(
+      map(products => products.find(p => p.id === +id)!)
+    );
   }
 }
