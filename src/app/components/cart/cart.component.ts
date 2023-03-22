55d407d975ee0b4b88365947990aeb87cc45cc6d
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-03-22 14:59:44 +0100

    modify: use ngModelChange for input validation

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -2,6 +2,7 @@ import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
 import { CartItem } from 'src/app/models/cart-item.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-cart',
@@ -21,6 +22,14 @@ export class CartComponent implements OnInit {
 
   removeFromCart(item: CartItem): void {
     this.cartService.removeFromCart(item);
+
+    Swal.fire({
+      icon: 'warning',
+      title: 'Item removed!',
+      text: `You have successfully it from the cart`,
+      confirmButtonText: 'OK'
+    });
+
   }
 
   getItemTotal(item: CartItem, quantity: number): number {
